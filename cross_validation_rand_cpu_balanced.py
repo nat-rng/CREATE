6fc2063ac2e2bca06fc34023f8107c8e7430adad
nat_rng <n.roongjirarat@gmail.com>
2023-07-17 19:19:54 +0100

    add balanced feature file

---
+++ cross_validation_rand_cpu_balanced.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import os
+from xgboost import XGBClassifier
+from sklearn.model_selection import RepeatedStratifiedKFold, RandomizedSearchCV, train_test_split
+import pickle
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+training_data_full= pd.read_parquet('data/parquet_files/training_data_rfm_balanced.parquet')
+
+xgb = XGBClassifier(n_jobs=-1)
+
+_, _, y_train_full, _ = train_test_split(training_data_full.drop(columns=['Flag']), training_data_full['Flag'], test_size=0.2, random_state=42)
+
+if not os.path.exists('models'):
+    os.makedirs('models')
+
+ten_fold = RepeatedStratifiedKFold(n_splits=10, random_state=42, n_repeats=5)
+
+X_train_sfs_xgb = pd.read_pickle('models/X_train_sfs_xgb_balanced.pkl')
+
+params = {
+    'n_estimators': [10, 50, 100, 200, 300], # number of trees
+    'eta': [0.01, 0.02, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5], # learning rate
+    'min_child_weight': [1, 5, 10], # minimum sum of instance weight (hessian) needed in a child
+    'max_depth': [3, 4, 5, 6, 7, 8], # maximum depth of a tree
+    'gamma': [0.0, 0.1, 0.2, 0.3, 0.4], # minimum loss reduction required to make a split
+    'subsample': [0.5, 0.6, 0.7, 0.8, 0.9, 1.0], # fraction of observations to be randomly samples for each tree.
+    'colsample_bytree': [0.6, 0.7, 0.8, 0.9, 1.0], # fraction of columns to be randomly samples for each tree.
+    'scale_pos_weight': [1, 2, 3, 4], # Control the balance of positive and negative weights
+    'reg_alpha': [0, 0.5, 1], # L1 regularization term on weight (analogous to Lasso regression)
+    'reg_lambda': [1, 1.5, 2], # L2 regularization term on weights (analogous to Ridge regression)
+    'random_state': [42] # seed used to generate reproducible results
+}
+
+# Use RandomizedSearchCV instead of GridSearchCV
+randomized_xgb = RandomizedSearchCV(xgb, param_distributions=params, cv=ten_fold, scoring='f1', 
+                                    return_train_score=True, n_jobs=-1, n_iter=8000, random_state=42)
+randomized_xgb.fit(X_train_sfs_xgb, y_train_full)
+
+print("Best Parameters {}".format(randomized_xgb.best_params_))
+print("Best Score {}".format(randomized_xgb.best_score_))
+
+with open('models/randomized_xgb_balanced.pkl', 'wb') as f:
+    pickle.dump(randomized_xgb, f)
