431165c2c3b4aa21165bd6e45ed65cdfe95f1380
nat_rng <n.roongjirarat@gmail.com>
2023-08-05 13:14:09 +0100

    create save points

--- verify_contracts.py
+++ verify_contracts.py
@@ -1,12 +1,14 @@
 from alchemy_api import AlchemyApi
 import pandas as pd
-import numpy as np
 import pickle
 import random
 from web3 import Web3
-# choose a Provider of your choice
-
 import concurrent.futures
+
+# Helper function to split a list into n parts
+def chunk_list(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
 
 alchemy = AlchemyApi()
 
@@ -20,6 +22,10 @@ user_addresses = pd.read_pickle('data/pickle_files/user_addresses.pkl')
 contract_addresses = pd.read_pickle('data/pickle_files/contract_addresses.pkl')
 
 check_addresses = list(set(check_addresses) - set(user_addresses) - set(contract_addresses))
+
+chunk_size = len(check_addresses) // 5
+
+address_chunks = list(chunk_list(check_addresses, chunk_size))
 
 def get_address_type(address, api_keys, alchemy_url):
     check_sum_address = Web3.to_checksum_address(address)
@@ -36,21 +42,27 @@ def api_key_rotation(api_keys):
 api_keys = alchemy.get_api_keys()
 alchemy_url = alchemy.get_api_url()
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-    future_to_address = {executor.submit(get_address_type, address, api_keys, alchemy_url): address for address in check_addresses}
-    for future in concurrent.futures.as_completed(future_to_address):
-        address = future_to_address[future]
-        try:
-            is_user_address = future.result()
-            if is_user_address:
-                user_addresses.append(address)
-            else:
-                contract_addresses.append(address)
-        except TypeError as e:
-            continue
+for i, chunk in enumerate(address_chunks):
+    print(f"Processing chunk {i + 1} of {len(address_chunks)}")
 
-with open('data/pickle_files/user_addresses.pkl', 'wb') as f:
-    pickle.dump(user_addresses, f)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        future_to_address = {executor.submit(get_address_type, address, api_keys, alchemy_url): address for address in chunk}
+        for future in concurrent.futures.as_completed(future_to_address):
+            address = future_to_address[future]
+            try:
+                is_user_address = future.result()
+                if is_user_address:
+                    user_addresses.append(address)
+                else:
+                    contract_addresses.append(address)
+            except TypeError as e:
+                continue
 
-with open('data/pickle_files/contract_addresses.pkl', 'wb') as f:
-    pickle.dump(contract_addresses, f)
+    # Save the updated lists after processing each chunk
+    with open('data/pickle_files/user_addresses.pkl', 'wb') as f:
+        pickle.dump(user_addresses, f)
+
+    with open('data/pickle_files/contract_addresses.pkl', 'wb') as f:
+        pickle.dump(contract_addresses, f)
+
+    print(f"Chunk {i + 1} processed and pickle files updated.")
