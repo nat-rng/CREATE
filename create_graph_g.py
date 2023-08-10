5a9f0cbbf6fcf8647033c21b49c7cef839b3ef6a
nat_rng <n.roongjirarat@gmail.com>
2023-08-10 17:21:01 +0100

    save graph file

---
+++ create_graph_g.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import networkx as nx
+import os
+
+if not os.path.exists('data/graph_files'):
+    os.makedirs('data/graph_files')
+
+eth_tx_df = pd.read_parquet('data/parquet_files/potential_fraud_transactions_df.parquet')
+eth_tx_df = eth_tx_df[eth_tx_df['to_id'].isnull()==False]
+eth_tx_df['to_id'] = eth_tx_df['to_id'].astype('int64')
+eth_tx_df = eth_tx_df.fillna(value={'asset_value': 0})
+eth_tx_df = eth_tx_df[eth_tx_df['asset'].isnull()==False]
+#encode asset column
+eth_tx_df['asset'] = eth_tx_df['asset'].astype('category')
+eth_tx_df = eth_tx_df[['from_id', 'to_id', 'asset_value', 'asset', 'category_id']]
+eth_tx_df = eth_tx_df[(eth_tx_df['asset']=='ETH') & (eth_tx_df['category_id']!=3)]
+eth_tx_df = eth_tx_df.fillna(value={'asset_value': 0})
+eth_tx_df['asset_value'] = eth_tx_df['asset_value'].astype('float')
+agg_eth_df = eth_tx_df.groupby(['from_id', 'to_id'], as_index=False).agg({'asset_value': 'sum'})
+
+G = nx.from_pandas_edgelist(agg_eth_df, 'from_id', 'to_id', edge_attr='asset_value', create_using=nx.DiGraph())
+
+with open('data/graph_files/eth_graph.gexf', 'wb') as f:
+    nx.write_gexf(G, f)
