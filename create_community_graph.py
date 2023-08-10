af342618288e42c21a42d652cc8011c555500ffe
nat_rng <n.roongjirarat@gmail.com>
2023-08-10 19:31:49 +0100

    update

--- create_community_graph.py
+++ create_community_graph.py
@@ -6,7 +6,7 @@ if not os.path.exists('data/graph_files'):
     os.makedirs('data/graph_files')
 
 partition_df = pd.read_parquet('data/graph_files/eth_partition_df.parquet')
-print(partition_df[partition_df['community_id'] == 1]['node_id'].tolist())
+print(len(partition_df[partition_df['community_id'] == 1]['node_id'].tolist()))
 G = nx.read_gexf('data/graph_files/eth_graph.gexf')
 print(len(G.nodes()))
 community_one = G.subgraph(partition_df[partition_df['community_id'] == 1]['node_id'].tolist())
