a84bc19004485971366b258def0d2934343eb4e5
nat_rng <n.roongjirarat@gmail.com>
2023-08-10 19:11:55 +0100

    new files

--- create_community_graph.py
+++ create_community_graph.py
@@ -5,8 +5,7 @@ import os
 if not os.path.exists('data/graph_files'):
     os.makedirs('data/graph_files')
 
-G = nx.read_gexf('data/graph_files/eth_graph.gexf')
-
 partition_df = pd.read_parquet('data/graph_files/eth_partition_df.parquet')
+G = nx.read_gexf('data/graph_files/eth_graph.gexf')
 community_one = G.subgraph(partition_df[partition_df['community_id'] == 1]['node_id'].tolist())
 nx.write_graphml(community_one, 'data/graph_files/eth_community_one.graphml')
