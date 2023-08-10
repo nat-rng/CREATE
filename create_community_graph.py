19ee8330de88fc4ee99ba6717ffab25c32ff6f03
nat_rng <n.roongjirarat@gmail.com>
2023-08-10 19:37:07 +0100

    update code

--- create_community_graph.py
+++ create_community_graph.py
@@ -5,10 +5,21 @@ import os
 if not os.path.exists('data/graph_files'):
     os.makedirs('data/graph_files')
 
+eth_tx_df = pd.read_parquet('data/parquet_files/all_eth_transactions_df.parquet')
+eth_tx_df = eth_tx_df[eth_tx_df['to_id'].isnull()==False]
+eth_tx_df['to_id'] = eth_tx_df['to_id'].astype('int64')
+eth_tx_df = eth_tx_df.fillna(value={'asset_value': 0})
+eth_tx_df = eth_tx_df[eth_tx_df['asset'].isnull()==False]
+#encode asset column
+eth_tx_df['asset'] = eth_tx_df['asset'].astype('category')
+eth_tx_df = eth_tx_df[['from_id', 'to_id', 'asset_value', 'asset', 'category_id']]
+eth_tx_df = eth_tx_df[(eth_tx_df['asset']=='ETH') & (eth_tx_df['category_id']!=3)]
+eth_tx_df = eth_tx_df.fillna(value={'asset_value': 0})
+eth_tx_df['asset_value'] = eth_tx_df['asset_value'].astype('float')
+agg_eth_df = eth_tx_df.groupby(['from_id', 'to_id'], as_index=False).agg({'asset_value': 'sum'})
+
+G = nx.from_pandas_edgelist(agg_eth_df, 'from_id', 'to_id', edge_attr='asset_value', create_using=nx.DiGraph())
+
 partition_df = pd.read_parquet('data/graph_files/eth_partition_df.parquet')
-print(len(partition_df[partition_df['community_id'] == 1]['node_id'].tolist()))
-G = nx.read_gexf('data/graph_files/eth_graph.gexf')
-print(len(G.nodes()))
 community_one = G.subgraph(partition_df[partition_df['community_id'] == 1]['node_id'].tolist())
-print(len(community_one.nodes()))
 nx.write_graphml(community_one, 'data/graph_files/eth_community_one.graphml')
